2de533cb8f520d78231b3505df0943844ffc0f97
Ben Border <ben@bunnbuns.net>
2019-10-22 14:46:16 -0400

    got the option for seasons working!
    yay

--- scripts/tv_show.js
+++ scripts/tv_show.js
@@ -2,11 +2,13 @@
 
 // --- Set vars --- //
 var id = getAllUrlParams().id;
-var season = "1";
-//season = getAllUrlParams().season;
+var season = getAllUrlParams().season;
+if(season == null){
+    season = "1";
+}
 var seasons = "";
-var eps ="";
-var titles="";
+var eps = "";
+var titles = "";
 window.onload = function() {
     //window is loaded, send requests
 }
@@ -14,24 +16,24 @@ window.onload = function() {
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.status == 200 || this.status == 302) {
-//go ahead and do stuff:
-    var tmp = JSON.parse(xhttp.responseText);
-  document.getElementById("desc").innerHTML = tmp.description;
-  document.getElementById("imdb_id").innerHTML = tmp.imdb_id;
-  document.getElementById("imdb_id").href = 'https://www.imdb.com/title/'+tmp.imdb_id;
-  document.getElementById("seasons").selectedIndex = tmp.season;
-  //document.getElementById("seasons").innerHTML = tmp.seasons[0];
+        //go ahead and do stuff:
+        var tmp = JSON.parse(xhttp.responseText);
+        document.getElementById("desc").innerHTML = tmp.description;
+        document.getElementById("imdb_id").innerHTML = tmp.imdb_id;
+        document.getElementById("imdb_id").href = 'https://www.imdb.com/title/'+tmp.imdb_id;
+        document.getElementById("seasons").innerHTML = "";
+        
+        var numSeasons = Object.keys(tmp.seasons).length;
+        for(var i = 0; i < numSeasons; i++) {
+            document.getElementById("seasons").innerHTML += "<option value="+tmp.seasons[i]+">" +tmp.seasons[i]+ "</option>"
+        }
+        document.getElementById("seasons").selectedIndex = tmp.season-1;
 
-    for(var key in tmp.seasons) {
-        seasons += "<option value="+tmp.seasons[key]+">" +tmp.seasons[key]+ "</option>"
-    }
-    document.getElementById("seasons").innerHTML = seasons;
-
-    var episodeNum = Object.keys(tmp.titles).length;
-    document.getElementById("list").innerHTML = "";
-    for(var i = 1; i < episodeNum+1; i++) {
-          //eps += i+'<br />';
-          document.getElementById("list").innerHTML += '<div class="ep"><h5>['+i+'] '+tmp.titles[i]+'</h5><img id="poster" style="max-width:100%;" src="'+tmp.thumbs[i]+'"></div>';
+        var episodeNum = Object.keys(tmp.titles).length;
+        document.getElementById("list").innerHTML = "";
+        for(var i = 1; i < episodeNum+1; i++) {
+              //eps += i+'<br />';
+              document.getElementById("list").innerHTML += '<div class="ep"><h5>['+i+'] '+tmp.titles[i]+'</h5><img id="poster" style="max-width:100%;" src="'+tmp.thumbs[i]+'"></div>';
         }
     }//end of request
 };
